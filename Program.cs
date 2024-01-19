5a7ff39d4928c79a84697bfe7b91b66a31bdd248
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-18 17:00:55 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -5,6 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string GRULower = "Gru";
+            string GRUUpper = "GRU";
+            string AGNESLower = "Agnes";
+            string AGNESUpper = "AGNES";
+            string MARGOLower = "Margo";
+            string MARGOUpper = "MARGO";
+            string VENDORLower = "Vendor";
+            string VENDORUpper = "VENDOR";
+
+            Console.WriteLine(AGNESUpper + " (O.s).");
+            Console.WriteLine("\"Oh my gosh look at that fluffy unicorn!\"\n");
+
+            Console.WriteLine("A FLUFFY UNICORN");
+            Console.WriteLine("is the main prize at the shooting gallery.\n");
+
+            Console.WriteLine(AGNESUpper);
+            Console.WriteLine("\"He's so fluffy I'm gonna die!!!\"\n");
         }
     }
 }
